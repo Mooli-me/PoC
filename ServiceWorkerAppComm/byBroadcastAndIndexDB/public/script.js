06ec02a890282c87b130645779a690875d646b46
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2021-08-06 20:30:15 +0200

    Issue #9 task 3 and fixes moving DB processes to service worker

--- ServiceWorkerAppComm/byBroadcastAndIndexDB/public/script.js
+++ ServiceWorkerAppComm/byBroadcastAndIndexDB/public/script.js
@@ -1,9 +1,7 @@
-const appVersion = 2;
 let secondsDiv;
 let clicksDiv;
 let button;
 let channel;
-let db;
 
 const clickMessage = {
     type: 'click',
@@ -15,100 +13,17 @@ function messageHandler (ev) {
     console.log('A new update from worker.')
     switch (ev.data.type) {
         case 'clicks':
-            element = clicksDiv
+            clicksDiv.innerText = ev.data.value;
             break;
         case 'time':
-            element = secondsDiv
+            secondsDiv.innerText = ev.data.value;
             break;
+        case 'DBUpdate':
+            console.log('DB update notification:', ev);
+            break;
+        default:
+            console.error('Unknown messaje:', ev);
     }
-    element.innerText = ev.data.value;
-}
-
-function updateDB (ev) {
-    const updateProcesses = [
-        (db) => {
-            console.log('There is no DB');
-        },
-        (db) => {
-            console.log('Create DB first version');
-            db.createObjectStore("times", { keyPath: "timestamp" });
-        },
-        (db) => {
-            console.log('Update to version 2');
-            db.createObjectStore("binaries", { keyPath: "sha256" });
-        },
-        (db) => {
-            console.log('Update to version 3');
-        }
-    ];
-
-    const db = ev.target.result;
-
-    if ( ev.newVersion > updateProcesses.length - 1 ) {
-        console.error(`Can't handle this DB version. Old version: ${ev.oldVersion}, new version:${ev.newVersion}.` );
-        return;
-    }
-
-    for ( let version = ev.oldVersion + 1 ; version <= ev.newVersion ; version ++ ) {
-        const process = updateProcesses[version];
-        process(db);
-    }
-    
-    //const objectStore = db.createObjectStore("name", { keyPath: "myKey" });
-}
-
-function openDB () {
-
-    if (!window.indexedDB) {
-        console.error("Browser doesn't seems to support IndexDB");
-        return;
-    }
-
-    const dbConnection = indexedDB.open('thingsDB', appVersion);
-
-    dbConnection.onupgradeneeded = updateDB;
-
-    dbConnection.onsuccess = (ev) => {
-        db = ev.target.result;
-        console.log('DB opened.')
-        console.log('DB stores:',db.objectStoreNames);
-    }
-
-    dbConnection.onerror = (ev) => console.error('Error opening DB:', ev.target.errorCode);
-
-}
-
-function addObjectToDB (obj, db, storeName) {
-
-    const transaction = db.transaction(storeName, 'readwrite');
-
-    transaction.oncomplete = ev => console.log('Transaction done:', ev);
-
-    transaction.onerror = ev => console.error('Transaction error:', ev);
-
-    const store = transaction.objectStore(storeName);
-
-    const resquest = store.add(obj)
-
-    resquest.onsuccess = (ev) => console.log('Time added to DB:', ev);
-
-}
-
-function writeDateToDB () {
-
-    const now = Date.now();
-
-    const obj = {
-        timestamp: now,
-        string: Date(now)
-    }
-
-    addObjectToDB(obj, db, 'times');
-
-}
-
-function startLoop () {
-    setInterval(writeDateToDB, 5000);
 }
 
 function main () {
@@ -127,14 +42,10 @@ function main () {
         channel.addEventListener('message', messageHandler);
         console.log('Opened channel in window');
 
-        openDB();
-
         secondsDiv = document.querySelector('div#seconds');
         clicksDiv = document.querySelector('div#clicks');
         button = document.querySelector('button');
         button.addEventListener('click', () =>  channel.postMessage(clickMessage) );
-
-        startLoop();
 
     } else {
 
--- ServiceWorkerAppComm/byBroadcastAndIndexDB/public/service-worker.js
+++ ServiceWorkerAppComm/byBroadcastAndIndexDB/public/service-worker.js
@@ -10,12 +10,113 @@ const precache = {
     '/img/logo.png'
   ]
 };
+const appVersion = 2;
 
+let db;
 
 let clickCounter = 0;
 let secondsCounter = 0;
 
 const channel = new BroadcastChannel('main');
+
+
+function updateDB (ev) {
+  const updateProcesses = [
+      (db) => {
+          console.log('There is no DB');
+      },
+      (db) => {
+          console.log('Create DB first version');
+          db.createObjectStore("times", { keyPath: "timestamp" });
+      },
+      (db) => {
+          console.log('Update to version 2');
+          db.createObjectStore("binaries", { keyPath: "sha256" });
+      },
+      (db) => {
+          console.log('Update to version 3');
+      }
+  ];
+
+  const db = ev.target.result;
+
+  if ( ev.newVersion > updateProcesses.length - 1 ) {
+      console.error(`Can't handle this DB version. Old version: ${ev.oldVersion}, new version:${ev.newVersion}.` );
+      return;
+  }
+
+  for ( let version = ev.oldVersion + 1 ; version <= ev.newVersion ; version ++ ) {
+      const process = updateProcesses[version];
+      process(db);
+  }
+  
+  //const objectStore = db.createObjectStore("name", { keyPath: "myKey" });
+}
+
+function openDB () {
+
+  if (!self.indexedDB) {
+      console.error("Browser doesn't seems to support IndexDB");
+      return;
+  }
+
+  const dbConnection = indexedDB.open('thingsDB', appVersion);
+
+  dbConnection.onupgradeneeded = updateDB;
+
+  dbConnection.onsuccess = (ev) => {
+      db = ev.target.result;
+      console.log('DB opened.')
+      console.log('DB stores:',db.objectStoreNames);
+  }
+
+  dbConnection.onerror = (ev) => console.error('Error opening DB:', ev.target.errorCode);
+
+}
+
+function addObjectToDB (obj, db, storeName) {
+
+  const transaction = db.transaction(storeName, 'readwrite');
+
+  transaction.oncomplete = ev => console.log('Transaction done:', ev);
+
+  transaction.onerror = ev => console.error('Transaction error:', ev);
+
+  const store = transaction.objectStore(storeName);
+
+  const resquest = store.add(obj)
+
+  resquest.onsuccess = (ev) => console.log('Time added to DB:', ev);
+
+  channel.postMessage(
+      {
+          type: 'DBUpdate',
+          storeName,
+          obj
+      }
+  );
+
+}
+
+function writeDateToDB () {
+
+  const now = Date.now();
+
+  const obj = {
+      timestamp: now,
+      string: Date(now)
+  }
+
+  addObjectToDB(obj, db, 'times');
+
+}
+
+function startLoop () {
+  setInterval(writeDateToDB, 5000);
+  console.log('Writing time to DB each 5 seconds.');
+  setInterval(updateSeconds,1000);
+  console.log('Updating time each second.')
+}
 
 function updateSeconds () {
   secondsCounter ++;
@@ -25,6 +126,9 @@ function updateSeconds () {
       value: secondsCounter
     })
 }
+
+
+openDB();
 
 console.log('Setting message event.')
 channel.addEventListener('message', (ev) => {
@@ -54,5 +158,4 @@ self.addEventListener('install', ev => {
 console.log('Getting control.')
 self.clients.claim();
 
-setInterval(updateSeconds,1000);
-console.log('Updating time each second.')
+startLoop();
